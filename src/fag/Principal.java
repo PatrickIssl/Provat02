722eebbe2b92d1bb646d7e29bc6f748464ce9bce
patrick <codeflowbr@gmail.com>
2025-09-02 21:36:55 -0300

    feature: trabalho concluido

--- src/fag/Principal.java
+++ src/fag/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git branch -M patrick-issler-dal-lago");
+		System.out.println("git branch -M patrick-issler");
 	}
 
 }
